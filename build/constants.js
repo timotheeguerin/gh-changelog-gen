98fd2d987a5ff8f555a90ec88cd0b86687c46889
Timothee Guerin <timothee.guerin@outlook.com>
2017-09-05 10:14:29 -0700

    Build

--- build/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.githubEndpoint = "https://api.github.com";
-exports.version = require("../package.json").version;
--- build/models.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IssueState;
-(function (IssueState) {
-    IssueState["open"] = "open";
-    IssueState["closed"] = "closed";
-})(IssueState = exports.IssueState || (exports.IssueState = {}));
